08fb92e7d65df0b2207c4a5b8b2d3fde810eb7d1
Michael Wydler <michael.wydler@googlemail.com>
2013-10-28 11:10:59 +0100

    Add getter and setter methods. Add empty test.

--- src/de/hrw/mdbt/model/Branch.java
+++ src/de/hrw/mdbt/model/Branch.java
@@ -11,5 +11,36 @@ public class Branch {
 	private String openingHours;
 	private String phone;
 	private ArrayList<Vehicle> vehicles;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public String getOpeningHours() {
+		return openingHours;
+	}
+	public void setOpeningHours(String openingHours) {
+		this.openingHours = openingHours;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public ArrayList<Vehicle> getVehicles() {
+		return vehicles;
+	}
+	public void setVehicles(ArrayList<Vehicle> vehicles) {
+		this.vehicles = vehicles;
+	}
 
 }
---
+++ test/de/hrw/mdbt/model/BranchTest.java
@@ -0,0 +1,79 @@
+package de.hrw.mdbt.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BranchTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOpeningHours() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOpeningHours() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPhone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPhone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetVehicles() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVehicles() {
+		fail("Not yet implemented");
+	}
+
+}
